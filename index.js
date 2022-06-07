57d53b3b2a6951620147193b7b00049f558dfcb1
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-07 15:33:36 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = "User"
+const welcomeMessage = "Welcome to Flatbook, "+currentUser+ "!"
+
+let myVar = "USER"
+
+let   excitedWelcomeMessage = "WELCOME TO FLATBOOK, "+myVar+" !"
+
+let    shortGreeting = "Welcome, "+currentUser[0]+"!"
